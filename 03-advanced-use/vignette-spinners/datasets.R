37df055b224ad7394e93de11936a0e5c4a54986c
Peter DeWitt <peter.dewitt@ucdenver.edu>
2019-04-23 11:49:58 -0600

    outline R package example

---
+++ 03-advanced-use/vignette-spinners/datasets.R
@@ -0,0 +1,32 @@
+# /*
+# file: datasets.R
+# author: Peter DeWitt
+#
+# This file will be sourced and evaluated with data-raw as the working directory
+# to build and save the data sets for the R package.  This file will also be
+# spun (knitr::spin) to build a .Rmd file which will be used as vignette.
+#
+# */
+#'---
+#'title: "Data Set Construction"
+#'author: "Peter DeWitt"
+#'output:
+#'  rmarkdown::html_vignette:
+#'    toc: true
+#'    number_sections: true
+#'vignette: >
+#'  %\VignetteIndexEntry{datasets}
+#'  %\VignetteEngine{knitr::rmarkdown}
+#'  %\VignetteEncoding{UTF-8}
+#'---
+#'
+#' 
+#'
+#' # NCHS - Leading Causes of Death: United States
+#'
+#' We download the data set
+
+
+https://catalog.data.gov/dataset/age-adjusted-death-rates-for-the-top-10-leading-causes-of-death-united-states-2013
+
+https://data.cdc.gov/api/views/bi63-dtpu/rows.csv?accessType=DOWNLOAD
